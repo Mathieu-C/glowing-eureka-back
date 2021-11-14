1d568007ace4c9a0df5eff62edbc09be90dfc5c3
extern-mathieu-chatillon <extern.mathieu.chatillon@vwfs.io>
2021-11-14 16:29:54 +0100

    Add Review validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
   validates :rating, :body, presence: true
+  validates :body, length: { minimum: 1, maximum: 2048 }
+  validates :rating, inclusion: { in: [1, 2, 3, 4, 5], message: "This rating is not allowed." }
 end
